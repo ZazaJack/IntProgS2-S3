4d66a9337706e772579fc224a9386878f72b8a1a
ZazaJack <rzahidbenjamin1@gmail.com>
2025-04-01 11:29:33 -0600

    Ejercicos de python

---
+++ temperatura.py
@@ -0,0 +1,7 @@
+temp_fah = float(input("Ingresa la temperatura en F: "))
+
+temp_cel = ((temp_fah - 32) * 5)/9
+temp_kel = temp_cel + 273.15
+
+print(f"Grados Celcius: {temp_cel:.2f}")
+print(f"Grados Kelvin: {temp_kel:.2f}")
